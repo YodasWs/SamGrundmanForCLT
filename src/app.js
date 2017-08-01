56b358d7aa62074ad063305cf6c783add83bdc5e
Sam Grundman <samuel@yodas.ws>
2017-08-01 17:13:35 -0400

    First simple little page

---
+++ src/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('myBooks', [
+	'ngRoute',
+])
+.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+	$locationProvider.html5Mode(true);
+	$routeProvider.when('/', {
+		templateUrl: 'pages/home.html',
+	})
+	.otherwise({redirectTo: '/'})
+}])
+.filter('strDate', () => {
+	return function(date) {
+		if (typeof date === 'string') {
+			date = new Date(date)
+		}
+		return date.toLocaleString(undefined, {
+			year: 'numeric',
+			month: 'short',
+			day: 'numeric',
+		})
+	}
+})
