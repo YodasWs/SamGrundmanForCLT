88173036c12599d6b7ee1d6f4f94a318fb6ee7b0
Sam Grundman <samuel@yodas.ws>
2017-08-04 10:23:51 -0400

    Add page footer

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('myBooks', [
+	'compSocialLinks',
 	'ngRoute',
 ])
 .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
---
+++ src/components/social-links/social-links.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('compSocialLinks')
+.component('socialLinks', {
+	templateUrl: 'components/social-links/social-links.html',
+	controller() {
+	}
+})
